762fdf315e925898250f1a2a564a4272fb48575b
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-21 03:55:28 +0000

    flatten code

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr, arr2) {
+    let i = 0;
+    if (arr.length !== arr2.length) {
+        return false;
+    } else {
+        while (i < arr.length) {
+            if (arr[i] === arr2[i]) {
+                i++;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+const assertArraysEqual = function(arr, arr2) {
+    if (eqArrays(arr, arr2)) {
+        console.log("The arrays match");
+    } else {
+        console.log("The arrays dont match");
+    }
+};
+
+const flatten = function(unflattened, finalArray = []) {
+    for (let i = 0; i < unflattened.length; i++) {
+        const currentArrEl = unflattened[i];
+        if (Array.isArray(currentArrEl)) {
+            flatten(currentArrEl, finalArray);
+        } else {
+            finalArray.push(currentArrEl);
+        }
+    }
+    return finalArray;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
