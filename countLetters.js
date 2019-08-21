b023b384a835280f7dcfa708d726a4f3fb8c7ce5
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-21 20:39:07 +0000

    countLetters code

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`😉😉😉Assertion Passed: [${actual}] === [${expected}]`);
+    } else {
+      console.log(`😭😭😭Assertion Failed: [${actual}] !== [${expected}]`);
+    }
+  
+  };
+
+const countLetters = function(string) {
+    let splitString = string.split('');
+    const results = {};
+    for (const letter of splitString){
+        if (letter !== ' '){
+            if (results[letter]) {
+                results[letter] += 1;
+            } else {
+                results[letter] = 1;
+            }
+        }
+    }   
+    return results;
+};
+
+console.log(countLetters("Hello world hello"));
+
+
+
