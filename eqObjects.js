99241dab0fd1fc2456a2d93d5941b015c53ab8fe
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-22 23:19:29 +0000

    all missing updates from lotide plus mapJS

--- eqObjects.js
+++ eqObjects.js
@@ -23,6 +23,16 @@ const eqArrays = function(arr, arr2) {
     return true;
 };
 
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+    if (eqObjects(object1, object2)) {
+        console.log("The objects match");
+    } else {
+        console.log("The objects dont match");
+    }
+};
+
+
 
 
 
@@ -56,18 +66,18 @@ const eqObjects = function(object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+assertObjectsEqual(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+assertObjectsEqual(ab, abc); // => false
 
 
 const cd = { c: "1", d: ["2", 3] }; 
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+assertObjectsEqual(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+assertObjectsEqual(cd, cd2); // => false
 
 
 
---
+++ map.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arr, arr2) {
+    let i = 0;
+    if (arr.length !== arr2.length) {
+        return false;
+    } else {
+        while (i < arr.length) {
+            if (arr[i] === arr2[i]) {
+                i++;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+const assertArraysEqual = function(arr, arr2) {
+    if (eqArrays(arr, arr2)) {
+        console.log("The arrays match");
+    } else {
+        console.log("The arrays dont match");
+    }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word[word.length-1])
+
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results1, ['g', 'c', 't', 'm']);
+assertArraysEqual(results1, ['d', 'l', 'o', 'r', 'm']);
+
