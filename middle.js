75371989e8fd6fb7938d48f91e517fed5367cec1
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-26 20:50:54 +0000

    middle refactoring and exporting

--- middle.js
+++ middle.js
@@ -1,26 +1,11 @@
-const eqArrays = function(arr, arr2) {
-    let i = 0;
-    if (arr.length !== arr2.length) {
-        return false;
-    } else {
-        while (i < arr.length) {
-            if (arr[i] === arr2[i]) {
-                i++;
-            } else {
-                return false;
-            }
-        }
-    }
-    return true;
-};
 
-const assertArraysEqual = function(arr, arr2) {
-    if (eqArrays(arr, arr2)) {
-        console.log("The arrays match");
-    } else {
-        console.log("The arrays dont match");
-    }
-};
+// const assertArraysEqual = function(arr, arr2) {
+//     if (eqArrays(arr, arr2)) {
+//         console.log("The arrays match");
+//     } else {
+//         console.log("The arrays dont match");
+//     }
+// };
 
 const middle = function(array) {
 let midArr = (array.length / 2);
@@ -38,5 +23,6 @@ let answer = [];
     }
 }
 
-console.log(middle([7,8,9,10,11,12]));
+module.exports = middle;
+// console.log(middle([7,8,9,10,11,12]));
 
