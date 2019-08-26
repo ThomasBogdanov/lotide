cfbcb929e0624d01b65a43644aa6004dc56cc4f6
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-26 23:18:37 +0000

    updated index with shorthand ES6

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+// const head   = require('./head');
+// const tail   = require('./tail');
+// const middle = require('./middle');
+
+
+const obj = { head: require('./head'), tail: require('./tail'), middle: require('./middle')};
+
+const {head, tail, middle} = obj;
 
 module.exports = {
   head:   head,
