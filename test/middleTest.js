d33b03f73cab87cc7bdffd2aba2cee68e51a193e
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-26 22:16:36 +0000

    middleTest and tailTest ChaiMochaTests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,15 @@
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(eqArrays(middle([7,8,9,10,11,12]), [9,10]), true);
+describe('#middle', () => {
+    it("should return [9,10], for [7,8,9,10,11,12]", () => {
+        assert.deepEqual(middle([7,8,9,10,11,12]), [9,10]);
+    });
+    it("should return [2], for [1,2,3]", () => {
+        assert.deepEqual(middle([1,2,3]), [2]);
+    });
+});
 
 // console.log(middle([7,8,9,10,11,12]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words).length, 2);
+describe('#tail', () => {
+    it("returns [2,3] for [1,2,3]",() => {
+        assert.deepEqual(tail([1,2,3]), [2,3]);
+    });
+});
+
+
+// // Test Case: Check the original array 
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// assertEqual(tail(words).length, 2);
