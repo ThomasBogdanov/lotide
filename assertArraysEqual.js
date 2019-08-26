bc9aceb8cd9b1310b372e39d624cb404a46285ed
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-26 20:37:11 +0000

    assertArraysEqual Test and refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(arr, arr2) {
-    let i = 0;
-    if (arr.length !== arr2.length) {
-        return false;
-    } else {
-        while (i < arr.length) {
-            if (arr[i] === arr2[i]) {
-                i++;
-            } else {
-                return false;
-            }
-        }
-    }
-    return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr, arr2) {
     if (eqArrays(arr, arr2)) {
@@ -21,3 +7,5 @@ const assertArraysEqual = function(arr, arr2) {
         console.log("The arrays dont match");
     }
 };
+
+module.exports = assertArraysEqual;
